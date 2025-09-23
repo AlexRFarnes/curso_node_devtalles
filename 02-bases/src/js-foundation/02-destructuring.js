4c7dbb5261f59a7f14e6102d049257cb74ecd4b6
AlexRFarnes <alexrfarnes@gmail.com>
2025-09-23 13:39:24 +0800

    02-bases refactor a usar TS

--- 02-bases/src/js-foundation/02-destructuring.js
+++
@@ -1,4 +0,0 @@
-// console.log(process.env.PORT ?? 3000);
-
-const { SHELL, HOME } = process.env;
-// console.table({ SHELL, HOME });
--- 02-bases/src/plugins/get-age.plugin.js
+++
@@ -1,6 +0,0 @@
-import getAgePlugin from "get-age";
-
-export const getAge = birthdate => {
-  if (!birthdate) return new Error("Birthdate is required");
-  return getAgePlugin(birthdate);
-};
